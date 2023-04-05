25b24d974a55d18e8b939b0b6128fd761f54dd20
Dina Abitova <d_abitova@kbtu.kz>
2023-04-05 23:40:23 +0600

    first commit

---
+++ paint.py
@@ -0,0 +1,108 @@
+import pygame
+#PAINT
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 700))
+    clock = pygame.time.Clock()
+    WHITE = (255, 255, 255)
+    RED = (225, 0, 50)
+    GREEN = (0, 225, 0)
+    BLUE = (0, 0, 225)
+ 
+    radius = 15
+    x = 0
+    y = 0
+    mode = 'blue'
+    points = []
+    
+    while True:
+        
+        pressed = pygame.key.get_pressed()
+        
+        alt_held = pressed[pygame.K_LALT] or pressed[pygame.K_RALT]
+        ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
+        
+        for event in pygame.event.get():
+            
+            # determin if X was clicked, or Ctrl+W or Alt+F4 was used IT FOR EXIT
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w and ctrl_held:
+                    return
+                if event.key == pygame.K_F4 and alt_held:
+                    return
+                if event.key == pygame.K_ESCAPE:
+                    return
+            
+                if event.key == pygame.K_r:
+                    mode = 'red'
+                elif event.key == pygame.K_g:
+                    mode = 'green'
+                elif event.key == pygame.K_b:
+                    mode = 'blue'
+
+
+            pressed = pygame.mouse.get_pressed()
+            pos = pygame.mouse.get_pos()
+            if pressed[0]:
+                pygame.draw.circle(screen, BLUE, pos, 5)
+            if i.type == pygame.MOUSEBUTTONDOWN:
+                if i.button == 1:
+                    pygame.draw.circle(
+                    screen, RED, i.pos, 20)
+                pygame.display.update()
+            elif i.button == 3:
+                pygame.draw.circle(
+                    screen, BLUE, i.pos, 20)
+                pygame.draw.rect(
+                    screen, RED,
+                    (i.pos[0] - 10,
+                     i.pos[1] - 10,
+                     20, 20))
+
+
+            elif i.button == 4:
+                screen.fill(WHITE)
+                pygame.display.update()
+
+
+            if event.type == pygame.MOUSEMOTION:
+                # if mouse moved, add point to list 
+                position = event.pos
+                points = points + [position]
+                points = points[-256:]
+                
+        screen.fill((255,255,255))
+        
+        # draw all points 
+        i = 0
+        while i < len(points) - 1:
+            drawLineBetween(screen, i, points[i], points[i + 1], radius, mode)
+            i += 1
+        
+        
+
+def drawLineBetween(screen, index, start, end, width, color_mode):
+    c1 = max(0, min(255, 2 * index - 256))
+    c2 = max(0, min(255, 2 * index))
+    
+    if color_mode == 'blue':
+        color = (c1, c1, c2)
+    elif color_mode == 'red':
+        color = (c2, c1, c1)
+    elif color_mode == 'green':
+        color = (c1, c2, c1)
+    
+    dx = start[0] - end[0]
+    dy = start[1] - end[1]
+    iterations = max(abs(dx), abs(dy))
+    
+    for i in range(iterations):
+        progress = 1.0 * i / iterations
+        aprogress = 1 - progress
+        x = int(aprogress * start[0] + progress * end[0])
+        y = int(aprogress * start[1] + progress * end[1])
+        pygame.draw.circle(screen, color, (x, y), width)
+        pygame.display.flip()
+        clock.tick(60)
